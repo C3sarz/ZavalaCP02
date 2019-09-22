078fec34376206459ca1317e418cc8d64c1e4171
Cesar Zavala <cesarz@ksu.edu>
2019-09-21 20:25:01 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ZavalaCP02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZavalaCP02.dir/main.c.o"
+  "ZavalaCP02.pdb"
+  "ZavalaCP02.exe"
+  "libZavalaCP02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ZavalaCP02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
