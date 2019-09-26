e86c4b4a4ce599079968d1bff5508c8b1d5bdf3c
Cesar Zavala <cesarz@ksu.edu>
2019-09-26 17:44:15 -0500

    startup

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
+/**********************************************
+* Name: Cesar Zavala *
+* Date: 10/06/2019 *
+* Assignment: Project 2 – Binary Trees *
+***********************************************
+* Provides functionality of a binary search tree. *
+***********************************************/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void Insert(int * num){
+
+
+}
+
+/**********************************************
+* Description: * Main function which does IO and provides the loop.
+* Input: *
+* Output: *
+***********************************************/
 int main() {
-    printf("Hello, World!\n");
+
+    ////START MAIN LOOP
+    int escape = 0;
+    while(!escape){
+printf("Enter (i)nstert, (s)earch, inorder (t)raversal, or (q)uit");
+
+
+
+
+
+
+    }
+    ////END MAIN LOOP
+
+
     return 0;
 }
